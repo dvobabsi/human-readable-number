4ac9c44317d89b9dde83af036c2ff74972886f93
dvobabsi <dvobabsi@gmail.com>
2023-02-13 21:52:48 +0200

    feat: add test one

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function toReadable (number) {
-  
+  const zero = ['zero','one','two','three','four','five','six','seven','eight','nine'];
+  const ten = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+  const twenty = ['', ' ','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+  const point = ['hundred','thousand']
+
+  let num = number.toString();
+
+  switch(num.length) {
+    case 1: //0-9
+        return zero[number];
+    case 2: //10-99
+        if(number < 20) {
+            return ten[number - 10];
+        } else {
+            return `${twenty[number[0]]}${twenty[1]}${twenty[number[1]]}`;
+        }
+    case 3: //100-999
+        return `999`;
+  }
 }
